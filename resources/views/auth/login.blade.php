6b5c4d56b6e6649e70cad7bcfd94fa90b9ceb4be
moji55 <spruc55@gmail.com>
2024-04-12 01:17:15 +0330

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FasaLUG / ثبت نام</title>
+        <title>FasaLUG /  ورود به حساب کاربری</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=poppins:500|vazirmatn:100,200,300,400,500,600,700,800,900"
@@ -21,7 +21,7 @@
     <body>
         <div class="container">
             <div class="w-full h-[100vh] flex flex-col justify-center items-center">
-                <div class="text-3xl font-bold mb-2">ثبت نام</div>
+                <div class="text-3xl font-bold mb-2">ورود</div>
                 <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" class="bg-white p-3 rounded min-w-[300px]" action="{{ route('login') }}">
@@ -29,14 +29,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('ایمیل')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('رمزعبور')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -50,19 +50,19 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('من را به یاد داشته باش') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('رمزعبور خود را فراموش کردید؟') }}
                 </a>
             @endif
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('ورود') }}
             </x-primary-button>
         </div>
     </form>
