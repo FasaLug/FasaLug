4bc050ada680061213e0108ff16886897ac4e67d
moji55 <spruc55@gmail.com>
2024-04-11 22:41:54 +0330

    Done. main page

---
+++ resources/views/panel/user.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>لاگ فسا / پنل کاربری</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=poppins:500|vazirmatn:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet" />
+    <!-- Styles -->
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<style>
+</style>
+
+<body id="app">
+    <div class="mx-3">
+        <div
+        class="nav mx-auto mt-3 shadow-sm px-4 bg-white rounded w-full min-h-[65px] max-w-[1440px] flex items-center justify-between">
+        <div class="text-xl font-bold select-none">لاگ فسا | گروه کاربران لینوکس فسا </div>
+        <div class="flex gap-3 select-none">
+            <div
+                class="bg-slate-100 hover:bg-slate-200 transition-colors text-black font-bold rounded p-2 flex items-center gap-1 cursor-pointer">
+                <iconify-icon width="1.3rem" icon="solar:user-linear"></iconify-icon>
+                <span>ورود/ثبت نام</span>
+
+            </div>
+
+            <div
+                class="bg-slate-100 hover:bg-slate-200 transition-colors text-black font-bold rounded p-2 flex items-center gap-1 cursor-pointer">
+                <iconify-icon width="1.3rem" icon="mdi:list-box-outline"></iconify-icon>
+                <span>رویداد های اخیر</span>
+
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+
+    <div class="pinglug shadow-lg relative select-none right-0 top-48 rounded-l-xl bg-white w-[800px] min-h-[30rem]">
+        <div class="pt-16 px-7 w-full">
+            <div class="font-semibold">
+                <div class="text-5xl">گروه کاربران لینوکس فسا</div>
+                <div class="w-[600px] pt-10">
+                    <div class="text-2xl">لاگ یا گروه کاربران لینوکس فسا گروهی مستقل و مردم نهاد است که در زمینه گنو / لینوکس و نرم افزار های آزاد فعالیت میکند</div>
+                </div>
+
+            </div>
+        </div>
+        <img class="h-[35rem] absolute top-[-.4rem] left-[-8rem]" src="/assets/pinguin.png" alt="">
+
+
+
+    </div>
+    <div class="container">
+
+    </div>
+    <div class="fade-in left"></div>
+    <div class="fade-in right"></div>
+</body>
+
+</html>
