348dd32197d1f8ff50b75166c7f7fcca675bacbf
moji55 <spruc55@gmail.com>
2024-04-12 14:25:18 +0330

    compelete user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,8 +61,11 @@
                             <a href="/myevents" class="relative p-6 rounded-2xl bg-white hover:scale-105 transition-all shadow">
                                 <div class="space-y-2">
                                     <div
-                                        class="flex items-center space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500 ">
+                                        class="flex items-center justify-between space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500">
                                         <span>رویداد های من</span>
+                                        <div class="flex items-center">
+                                            <iconify-icon width="2.1rem" icon="mdi:events"></iconify-icon>
+                                        </div>
                                     </div>
 
                                     <div class="flex justify-evenly gap-2">
@@ -89,57 +92,24 @@
                                 </div>
                             </a>
 
-                            <div class="relative p-6 rounded-2xl bg-white shadow ">
+                            <a href="profile/edit" class="relative p-6 rounded-2xl bg-white hover:scale-105 transition-all shadow">
                                 <div class="space-y-2">
                                     <div
-                                        class="flex items-center space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500">
-                                        <span>New customers</span>
+                                        class="flex items-center justify-between space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500">
+                                        <span>پروفایل کاربری</span>
+                                        <div class="flex items-center">
+                                            <iconify-icon width="2.1rem" icon="tabler:user-edit"></iconify-icon>
+                                            {{-- <img class="w-[70px] h-[70px] rounded-full shadow-xl" src="/assets/pinguin.png" alt=""> --}}
+                                        </div>
+
                                     </div>
 
-                                    <div class="text-3xl">
-                                        1340
-                                    </div>
-
-                                    <div class="flex items-center space-x-1 rtl:space-x-reverse text-sm font-medium text-red-600">
-
-                                        <span>3% decrease</span>
-
-                                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                            aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                d="M12 13a1 1 0 100 2h5a1 1 0 001-1V9a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586 3.707 5.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
+                                    <div class="text-2xl">
+                                        {{ auth()->user()->name }}
                                     </div>
                                 </div>
 
-                            </div>
-
-                            <div class="relative p-6 rounded-2xl bg-white shadow">
-                                <div class="space-y-2">
-                                    <div
-                                        class="flex items-center space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500 ">
-
-                                        <span>New orders</span>
-                                    </div>
-
-                                    <div class="text-3xl">
-                                        3543
-                                    </div>
-
-                                    <div class="flex items-center space-x-1 rtl:space-x-reverse text-sm font-medium text-green-600">
-
-                                        <span>7% increase</span>
-
-                                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                            aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </div>
-                                </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
